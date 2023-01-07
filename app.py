9c76249c9c2a00f2a35f8913275e4a31a639fb6f
h-atsu <jhuim08@gmail.com>
2023-01-07 15:10:15 +0900

    add seclection of cafeteria

--- app.py
+++ app.py
@@ -2,9 +2,22 @@ import streamlit as st
 from problem import MenuProblem
 import matplotlib.pyplot as plt
 import pandas as pd
+from PIL import Image
 
 
-df = pd.read_csv('menu.csv')
+image = Image.open('asset/icon.png')
+st.set_page_config(
+    page_title="Menu Opt",
+    page_icon=image,
+    menu_items={
+        'Get Help': 'https://www.google.com',
+        'Report a bug': "https://www.google.com",
+        'About': """
+         # 学食献立最適化
+         整数計画問題を解いて栄養バランスを考慮した献立を提案します
+         """
+    })
+
 
 # 　一食の栄養必要量を入れた辞書
 require_dic = {
@@ -19,7 +32,19 @@ require_dic = {
 
 # タイトル
 st.title('学食の献立最適化')
-# 純粋なテキスト
+
+cafeteria_list = ['図書館下食堂', 'かさね', 'レインボー', 'ファミール']
+cafeteria_dict = {
+    '図書館下食堂': 'kansita.csv',
+    'かさね': 'kasane.csv',
+    'レインボー': 'rainbow.csv',
+    'ファミール': 'famille.csv'
+}
+file_name = cafeteria_dict[st.selectbox(
+    label="食堂を選んでください", options=cafeteria_dict.keys())]
+df = pd.read_csv('./data/' + file_name)
+
+# 予算と好き嫌いリストを入力
 max_price = st.number_input('予算を教えてください', 100, 2000, 500, step=50)
 T_plus = st.multiselect("食べたい商品を選択", df['name'])
 T_minus = st.multiselect("食べたくない商品を選択", df['name'])
--- scraiping.py
+++ scraiping.py
@@ -6,62 +6,6 @@ import requests
 import pandas as pd
 import re
 
-driver = webdriver.Chrome()
-driver.get('https://west2-univ.jp/sp/menu.php?t=663255')
-
-time.sleep(0.5)
-toggle_titles = driver.find_elements(By.CLASS_NAME, "toggleTitle")
-toggle_titles_sub = driver.find_elements(By.CLASS_NAME, "open")
-
-print(toggle_titles)
-print(toggle_titles_sub)
-
-for v in reversed(toggle_titles):
-    if v in toggle_titles_sub:
-        continue
-    v.click()
-
-time.sleep(0.5)
-
-html = driver.page_source.encode("utf-8")  # HTMLのエンコード
-bsObj = BeautifulSoup(html, "html.parser")
-
-driver.close()
-driver.quit()
-
-cont = bsObj.find(class_='area')
-all_menu_links = cont.find_all('a')
-
-base_url = 'https://west2-univ.jp/sp/'
-
-columns = ['price', 'energy', 'protein', 'fat',
-           'carbohydrates', 'salt', 'calcium', 'vegetable']
-
-menu_data = []
-
-for link in all_menu_links:
-    time.sleep(0.5)
-    print(base_url + link.get('href'))
-    menu_page = requests.get(base_url + link.get('href'))
-    menu_soup = BeautifulSoup(menu_page.text, 'html.parser').find(id='main')
-    menu_name = menu_soup.find('h1')
-    menu_detail = menu_soup.find(class_="detail")
-    menu_detail_list = menu_detail.find_all("li")
-
-    # 各栄養素を記入
-    menu_dic = {'name': menu_name.text}
-    for i, col in enumerate(columns):
-        menu_dic[f'{col}'] = menu_detail_list[i].find(
-            'span', class_='price').text
-    # menu画像のurl
-    menu_dic['img_url'] = menu_soup.find('img')['src']
-
-    menu_data.append(menu_dic)
-
-# data frame operations
-
-df = pd.DataFrame(menu_data)
-
 
 def del_str(S):
     for i, c in enumerate(S):
@@ -70,19 +14,93 @@ def del_str(S):
     return S
 
 
-def f1(x): return float(x[:-1])
-def f2(x): return float(x[:-2])
-def f4(x): return float(x[:-4])
+def get_data(url, save_path):
+    driver = webdriver.Chrome()
+    driver.get(url)
+
+    time.sleep(5)
+    toggle_titles = driver.find_elements(By.CLASS_NAME, "toggleTitle")
+    toggle_titles_sub = driver.find_elements(By.CLASS_NAME, "open")
+
+    for v in reversed(toggle_titles):
+        if v in toggle_titles_sub:
+            continue
+        v.click()
+    time.sleep(5)
+    html = driver.page_source.encode("utf-8")  # HTMLのエンコード
+    bsObj = BeautifulSoup(html, "html.parser")
+
+    driver.close()
+    driver.quit()
+
+    cont = bsObj.find(class_='area')
+    all_menu_links = cont.find_all('a')
+
+    base_url = 'https://west2-univ.jp/sp/'
+
+    columns = ['price', 'energy', 'protein', 'fat',
+               'carbohydrates', 'salt', 'calcium', 'vegetable']
+
+    menu_data = []
+
+    for link in all_menu_links:
+        time.sleep(0.5)
+        print(base_url + link.get('href'))
+        menu_page = requests.get(base_url + link.get('href'))
+        menu_soup = BeautifulSoup(
+            menu_page.text, 'html.parser').find(id='main')
+        menu_name = menu_soup.find('h1')
+        menu_detail = menu_soup.find(class_="detail")
+        menu_detail_list = menu_detail.find_all("li")
+
+        # 各栄養素を記入
+        menu_dic = {'name': menu_name.text}
+        for i, col in enumerate(columns):
+            menu_dic[f'{col}'] = menu_detail_list[i].find(
+                'span', class_='price').text
+        # menu画像のurl
+        menu_dic['img_url'] = menu_soup.find('img')['src']
+
+        menu_data.append(menu_dic)
+
+    def fi(i):
+        return lambda x: x[:-i]
+    # def f2(x): return float(x[:-2])
+    # def f4(x): return float(x[:-4])
+
+    # data frame operations
+    df = pd.DataFrame(menu_data)
+    df['name'] = df['name'].map(del_str)
+    print(len(df))
+    # 単位を削除
+    df['price'] = df['price'].map(fi(1))
+    df['energy'] = df['energy'].map(fi(4))
+    df['protein'] = df['protein'].map(fi(1))
+    df['fat'] = df['fat'].map(fi(1))
+    df['carbohydrates'] = df['carbohydrates'].map(fi(1))
+    df['salt'] = df['salt'].map(fi(1))
+    df['calcium'] = df['calcium'].map(fi(2))
+    df['vegetable'] = df['vegetable'].map(fi(1))
+
+    # 数値変換
+    cols = ['price', 'energy', 'protein', 'fat',
+            'carbohydrates', 'salt', 'calcium', 'vegetable']
+    df[cols] = df[cols].apply(pd.to_numeric, errors='coerce', axis=1)
+    df = df.dropna()
+
+    df.to_csv(save_path, index=False)
+    print(f"menu size is {len(df)}")
 
 
-df['name'] = df['name'].map(del_str)
-df['price'] = df['price'].map(f1)
-df['energy'] = df['energy'].map(f4)
-df['protein'] = df['protein'].map(f1)
-df['fat'] = df['fat'].map(f1)
-df['carbohydrates'] = df['carbohydrates'].map(f1)
-df['salt'] = df['salt'].map(f1)
-df['calcium'] = df['calcium'].map(f2)
-df['vegetable'] = df['vegetable'].map(f1)
+if __name__ == '__main__':
+    cafeteria_dict = {
+        'rainbow': 'https://west2-univ.jp/sp/menu.php?t=663251',
+        'kansita': 'https://west2-univ.jp/sp/menu.php?t=663252',
+        'famille': 'https://west2-univ.jp/sp/menu.php?t=663255',
+        'kasane': 'https://west2-univ.jp/sp/menu.php?t=663258',
+    }
 
-df.to_csv('menu.csv', index=False)
+    for k, v in cafeteria_dict.items():
+        print(f'===== {k} scraping start =====')
+        get_data(v, 'data/' + k + '.csv')
+        print('===== finish =====')
